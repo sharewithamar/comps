1d7d4ad845b6ee965ea54528c44954a5dc73d95e
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-03-11 11:47:59 +0530

    divider component - usage of ng-content

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { ElementsRoutingModule } from "./elements-routing.module";
+import { SharedModule } from "../shared/shared.module";
 import { ElementsHomeComponent } from "./elements-home/elements-home.component";
-import { PlaceholderComponent } from './placeholder/placeholder.component';
-import { TimesDirective } from './times.directive';
+import { PlaceholderComponent } from "./placeholder/placeholder.component";
+import { TimesDirective } from "./times.directive";
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: []
 })
 export class ElementsModule {}
