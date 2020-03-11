ccfeac8aa4e23586ce1e03a65c41c50d973b7617
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-03-11 14:18:42 +0530

    segment component with ng-content select and hanlding empty

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from "../shared/shared.module";
 import { ElementsHomeComponent } from "./elements-home/elements-home.component";
 import { PlaceholderComponent } from "./placeholder/placeholder.component";
 import { TimesDirective } from "./times.directive";
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective, SegmentComponent],
   imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: []
 })
