97fdd0ee375c6c034b4ea8d105a8cd003fb87ba2
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-03-16 16:38:03 +0530

    item list component and usage of :host:not(:first-of-type)

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -11,6 +11,19 @@ export class ViewsHomeComponent implements OnInit {
     { value: 10000, label: "Runs" },
     { value: 45, label: "Avg" }
   ];
+
+  items = [
+    {
+      image: "assets/images/couch.jpeg",
+      title: "couch",
+      description: "Fantastic Couch"
+    },
+    {
+      image: "assets/images/dresser.jpeg",
+      title: "Dresser",
+      description: "Fantastic Dresser"
+    }
+  ];
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { ViewsRoutingModule } from "./views-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 import { StatisticsComponent } from "./statistics/statistics.component";
 import { ViewsHomeComponent } from "./views-home/views-home.component";
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
-  declarations: [StatisticsComponent, ViewsHomeComponent],
-  imports: [CommonModule, ViewsRoutingModule]
+  declarations: [StatisticsComponent, ViewsHomeComponent, ItemListComponent],
+  imports: [CommonModule, ViewsRoutingModule, SharedModule]
 })
 export class ViewsModule {}
