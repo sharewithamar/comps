ff1628f164b12804351574a7e9dfbe3e28b8f2f4
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-03-16 11:14:13 +0530

    child component routing

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,9 +5,12 @@ import { CollectionsRoutingModule } from "./collections-routing.module";
 import { SharedModule } from "../shared/shared.module";
 import { CollectionsHomeComponent } from "./collections-home/collections-home.component";
 import { TableComponent } from "./table/table.component";
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: []
 })
