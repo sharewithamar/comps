25358c5c82760915379579a91ff825b87244c4d2
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-03-18 16:56:31 +0530

    displaying modal by appending to body through elementRef

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"]
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.append(this.el.nativeElement);
   }
-
 }
